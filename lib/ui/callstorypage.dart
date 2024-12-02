e0fb6598c5da0bf01e410582c6d081b0aa36f2a3
baptt123 <21130529@st.hcmuaf.edu.vn>
2024-12-02 21:16:24 +0700

    up code(2/12/2024)

---
+++ lib/ui/callstorypage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:app_chat/ui/storypage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Story Viewer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StoryPage(),
+    );
+  }
+}
