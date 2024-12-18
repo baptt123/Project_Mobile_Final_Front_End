31bcb1030b267f3dab805da39648fad856e254ff
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2024-12-18 22:01:10 +0700

    up code cho phan giao dien chinh(18/12/2024)

---
+++ lib/api/callingapi.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../dto/notificationsdto.dart';
+import '../dto/postdto.dart';
+import '../dto/storydto.dart';
+class CallingAPI {
+  static const String postURL = 'http://192.168.5.248:8080/api/post/getpost'; // URL API
+  static const String storyURL= 'http://192.168.5.248:8080/api/story/getstories';
+  static const String notificationURL='http://192.168.5.248:8080/api/notification/get-notification';
+  static Future<List<PostDTO>> fetchPosts() async {
+    final response = await http.get(Uri.parse(postURL));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => PostDTO.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+  // Phương thức GET: Lấy danh sách StoryDTO
+  static Future<List<StoryDTO>> fetchStories() async {
+    final response = await http.get(Uri.parse(storyURL));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => StoryDTO.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load stories');
+    }
+  }
+  static Future<List<NotificationsDTO>> fetchNotifications() async{
+    final response = await http.get(Uri.parse(notificationURL));
+    if(response.statusCode==200){
+      final List<dynamic> data=json.decode(response.body);
+      return data.map((json)=> NotificationsDTO.fromJson(json)).toList();
+    }else{
+      throw Exception("Failed to load notifications");
+    }
+  }
+}
